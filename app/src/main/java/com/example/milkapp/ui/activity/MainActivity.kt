3bb0dfb30433af70318fa75a42f6534d31c1fd74
abc <innovagicgithub@gmail.com>
2023-02-26 11:43:40 +0500

    my commit

---
+++ app/src/main/java/com/example/milkapp/ui/activity/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.milkapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.milkapp.R
+import com.example.milkapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding :ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        githubFun()
+    }
+
+    fun githubFun(){
+        print("fuck it...")
+    }
+}
---
+++ app/src/main/java/com/example/milkapp/ui/activity/MainMenu.kt
@@ -0,0 +1,12 @@
+package com.example.milkapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.milkapp.R
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
